83a29b988d811084e86bd785f7d2992b4fedac14
abdulhadicse <abdulhadi.aust@gmail.com>
2022-07-02 22:27:46 +0600

    two sum

---
+++ Array/two_sum.php
@@ -0,0 +1,25 @@
+<?php
+
+// 1. Two Sum
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $map_index = [];
+        
+        for ( $i = 0; $i < count( $nums ); $i++ ) {
+            $search_number = $target - $nums[$i];
+            
+            if ( array_key_exists( $search_number, $map_index ) ) {
+                return [ $map_index[$search_number], $i ];
+            } else {
+                $map_index[ $nums[$i] ] = $i;
+            }
+        }
+    }
+}
